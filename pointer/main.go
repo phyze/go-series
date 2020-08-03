55e566569a342b01f96bf29f6f1dc73244f6a4c7
General Sandra <kittisak.pe@appworks.co.th>
2020-08-04 01:27:54 +0700

    update pointer

--- pointer/main.go
+++ pointer/main.go
@@ -5,8 +5,12 @@ import (
 )
 
 func main() {
-	fmt.Println("--- pointer ---")
+	fmt.Println("-- represent pointer how it work")
 	printAddress(5)
+	fmt.Println()
+
+	fmt.Println("-- how to use pointer with struct")
+	howToUsePtrWithStrcut()
 }
 
 func printAddress(n int) {
@@ -17,4 +21,27 @@ func printAddress(n int) {
 	fmt.Printf("a pointed to %v , address is %p \n", a, &a)
 	fmt.Printf("value of b that kept address of a , value is %p  \n", b)
 	fmt.Printf("b pointed to  %p  , address is %p \n", b, &b)
+}
+
+func howToUsePtrWithStrcut() {
+	type Person struct {
+		Name string
+		Age  uint8
+	}
+
+	type Employee struct {
+		ID     string
+		Person *Person
+	}
+
+	person := &Person{
+		Name: "nanao chan",
+		Age:  18,
+	}
+
+	employee := &Employee{
+		Person: person,
+		ID:     "ACG10101",
+	}
+	fmt.Println(employee)
 }
