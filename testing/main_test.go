606fa95200606a27ce3e22bc1e9472c1dc9d3873
General Sandra <kittisak.pe@appworks.co.th>
2020-08-04 14:21:25 +0700

    update

--- testing/main_test.go
+++ testing/main_test.go
@@ -10,7 +10,6 @@ func Test_plus_func_must_eq_expected_must_pass(t *testing.T) {
 	}
 }
 
-
 func Test_plus_func_must_not_eq_expected_must_pass(t *testing.T) {
 	expected := 5
 	actual := plus(2, 2)
@@ -20,3 +19,21 @@ func Test_plus_func_must_not_eq_expected_must_pass(t *testing.T) {
 }
 
 func plus(a, b int) int { return a + b }
+
+func Prime(number uint) bool {
+	switch number {
+	case 0:
+		return false
+	case 1:
+		return false
+	}
+	var i uint = 2
+	for i < number {
+		if number%i == 0 {
+			return false
+		}
+		i++
+	}
+	return true
+}
+
